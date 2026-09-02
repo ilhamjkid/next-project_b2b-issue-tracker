90f3e09574382723eabd27e8268f523ac119e5e3
Ilham Jaya Kusuma <ilhamjkid@gmail.com>
2026-09-02 17:06:43 +0700

    refactor: disable default browser autocomplete on inputs and textareas

--- src/features/tickets/components/ticket-filter.tsx
+++ src/features/tickets/components/ticket-filter.tsx
@@ -75,7 +75,6 @@ export function TicketFilter() {
           }}
           value={localSearchValue}
           disabled={isPending}
-          autoComplete="off"
         />
         <InputGroupAddon>
           <SearchIcon />
